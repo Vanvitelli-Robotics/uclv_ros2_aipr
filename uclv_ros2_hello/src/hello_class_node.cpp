1323fa215528d0812e32383a89c9c8fbd26586c7
Marco Costanzo <marco.costanzo@unicampania.it>
2024-04-05 09:58:31 +0200

    first commit

---
+++ uclv_ros2_hello/src/hello_class_node.cpp
@@ -0,0 +1,50 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include <rclcpp/rclcpp.hpp>
+
+namespace uclv
+{
+
+class HelloNode : public rclcpp::Node
+{
+private:
+  rclcpp::TimerBase::SharedPtr timer_;
+  int count_;
+
+public:
+  HelloNode(const rclcpp::NodeOptions& options = rclcpp::NodeOptions())
+    : rclcpp::Node("filter_msg_node", options), count_(0)
+  {
+    timer_ = this->create_wall_timer(std::chrono::milliseconds(1000), std::bind(&HelloNode::timer_callback, this));
+
+    // alternative using operator""ms
+    // using namespace std::chrono_literals;  // needed to access operator(suffix) ms
+    // timer_ = this->create_wall_timer(500ms, std::bind(&HelloNode::timer_callback, this));
+  }
+
+  ~HelloNode() = default;
+
+  void timer_callback()
+  {
+    count_++;
+    RCLCPP_INFO_STREAM(this->get_logger(), "Hello World " << count_);
+  }
+};
+
+}  // namespace uclv
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+
+  auto hello_node = std::make_shared<uclv::HelloNode>();
+
+  rclcpp::spin(hello_node);
+
+  rclcpp::shutdown();
+
+  return 0;
+}
---
+++ uclv_ros2_hello/src/hello_noclass_node.cpp
@@ -0,0 +1,43 @@
+#include <chrono>
+#include <memory>
+#include <string>
+
+#include <rclcpp/rclcpp.hpp>
+
+/* We do not recommend this style anymore, because composition of multiple
+ * nodes in the same executable is not possible. Please see one of the subclass
+ * examples for the "new" recommended styles. This example is only included
+ * for completeness because it is similar to "classic" standalone ROS nodes. */
+
+int main(int argc, char * argv[])
+{
+  // initialize ROS2
+  rclcpp::init(argc, argv);
+
+  // The Node object
+  auto node = rclcpp::Node::make_shared("hello");
+
+  // the variable
+  int count = 0;
+
+  // the rate
+  rclcpp::WallRate loop_rate(std::chrono::milliseconds(1000));
+
+  // the main loop
+  while (rclcpp::ok()) {
+
+    // to stuff
+    count ++;
+    RCLCPP_INFO_STREAM(node->get_logger(), "Hello World " << count);
+
+    // spin is used to receive messages/service requests
+    rclcpp::spin_some(node);
+
+    // sleep on rate
+    loop_rate.sleep();
+  }
+
+  // exit
+  rclcpp::shutdown();
+  return 0;
+}
